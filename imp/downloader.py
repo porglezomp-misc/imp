f99cca9434f391eb3cc896c5ad8d3bd13b7eab06
Caleb Jones <self@calebjones.net>
2016-02-13 12:53:22 -0500

    Instead of downloading images, record them in the database
    Later, download any images which aren't backed by a local file

--- imp/downloader.py
+++ imp/downloader.py
@@ -1,11 +1,19 @@
+from __future__ import print_function
+
 import requests
 import bs4
 import sys
 import logging
 import os
 import sys
+import sqlite3
 
+import database
+
+
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+db = None
 
 
 def download_image(url, location):
@@ -19,6 +27,18 @@ def download_image(url, location):
     logger.info("Finished downloading %s to %s", url, location)
 
 
+def record_image(url, title):
+    f = url.split('/')[-1]
+    key = f.split('.')[0]
+    i = db.execute('SELECT id FROM images WHERE key = ?', [key])
+    if i.fetchone() is None:
+        with db:
+            logger.info("INSERT INTO images (name, key, url) "
+                        "VALUES ('%s', '%s', '%s');", title, key, url)
+            db.execute('INSERT INTO images (name, key, url) '
+                       'VALUES (?, ?, ?);', (title, key, url))
+
+
 def locate_image(url, save_into=''):
     r = requests.get(url)
     html = bs4.BeautifulSoup(r.text, 'html.parser')
@@ -26,25 +46,44 @@ def locate_image(url, save_into=''):
     for img in imgs:
         url = 'https:' + img['src'].rstrip('?0123456789')
         logger.info("Downloading %s", url)
-        title = url.lstrip('https://i.imgur.com/')
-        location = os.path.join(save_into, title)
-        download_image(url, location)
+        title = html.select('h1.post-title')[0].text
+        record_image(url, title)
 
 
-def download_many(source, dest):
+def find_many(source, dest):
     if not os.path.isdir(dest):
         os.mkdir(dest)
 
     r = requests.get(source)
     html = bs4.BeautifulSoup(r.text, 'html.parser')
     links = html.find_all('a', class_='image-list-link')
+    keys = {row[0] for row in db.execute('SELECT key FROM images;').fetchall()}
 
     for link in links:
-        url = 'https://imgur.com' + link['href']
-        # TODO: Concurrent processing
-        locate_image(url, save_into=dest)
+        urlkey = link['href'].split('/')[-1].split('.')[0]
+        if urlkey in keys:
+            logger.info('%s already downloaded, skipping', link['href'])
+        else:
+            url = 'https://imgur.com' + link['href']
+            # TODO: Concurrent processing
+            locate_image(url, save_into=dest)
 
+
+RESOURCES = 'resources'
 
 if __name__ == '__main__':
     # TODO: Command line interface
-    download_many(sys.argv[1], 'out')
+    db = database.make_db('imp.db')
+    find_many(sys.argv[1], 'out')
+    not_downloaded = db.execute('SELECT * FROM images WHERE file IS NULL;')
+    if not os.path.isdir(RESOURCES):
+        os.mkdir(RESOURCES)
+    for image in not_downloaded.fetchall():
+        fname = image['url'].split('/')[-1]
+        fname = os.path.join(RESOURCES, fname)
+        download_image(image['url'], fname)
+        with db:
+            db.execute('UPDATE images SET file = ? WHERE id = ?',
+                       (fname, image['id']))
+            logger.info("UPDATE images SET file = '%s' WHERE id = %s",
+                       fname, image['id'])
