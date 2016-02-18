838a027474c18bd70094e015fa23d9ba20c933d4
Caleb Jones <self@calebjones.net>
2016-02-18 16:12:53 -0500

    Directly wrap the sqlite3 database methods

--- imp/database.py
+++ imp/database.py
@@ -1,5 +1,15 @@
 import migrations
 
 
-def Database(name):
-    return migrations.migrate_db(name)
+class Database(object):
+    def __init__(self, name):
+        self.db = migrations.migrate_db(name)
+
+    def execute(self, text, *items):
+        return self.db.execute(text, *items)
+
+    def __enter__(self, *args):
+        self.db.__enter__(*args)
+
+    def __exit__(self, *args):
+        self.db.__exit__(*args)
