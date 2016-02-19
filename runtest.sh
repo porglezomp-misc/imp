46e1e4acc26fffb735254952890b8777bc52981d
Caleb Jones <self@calebjones.net>
2016-02-19 01:56:06 -0500

    Add tests for creating images
    Improve the run script

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-(cd imp && python server.py &> test_server.log) &
-sleep 1
+pushd imp
+python server.py &> test_server.log &
+popd
+echo $! > test.pid
+sleep 0.1
 casperjs test tests/
-kill $!
+kill %%
--- tests/imptest.js
+++ tests/imptest.js
@@ -1,6 +1,29 @@
-casper.test.begin('Test that all routes return 200', function(test) {
+casper.test.begin('Test the critical paths of the application', function(test) {
     casper.start('http://localhost:8888/', function() {
         test.assertHttpStatus(200, 'The root page should load');
+        test.assertExists('a[href="/images/new"]',
+                          'The page should have a link to add a new image');
+        this.click('a[href="/images/new"]');
+    });
+
+    casper.waitForUrl('http://localhost:8888/images/new', function() {
+        test.assertHttpStatus(200, 'The new images page should load');
+        test.assertExists('form[action="/images/new"]',
+                          'The page should have a form to describe the image');
+        this.fill('form[action="/images/new"]', {
+            name: 'My Steven Universe Poster!',
+            url: 'http://i.imgur.com/Cy0QxaE.jpg',
+            description: 'An extremely cool poster',
+        }, true);
+    });
+
+    casper.waitForUrl('http://localhost:8888/images/Cy0QxaE', function() {
+        test.assertHttpStatus(200, 'The newly created image page should load');
+        test.assertTitle('My Steven Universe Poster!',
+                         'The page should have the title we entered');
+        // TODO: The header should also have the title we entered
+        test.assertExists('img[src="http://i.imgur.com/Cy0QxaE.jpg"]',
+                          'The image we chose should be on the page');
     });
 
     casper.thenOpen('http://localhost:8888/images/', function() {
@@ -12,7 +35,7 @@ casper.test.begin('Test that all routes return 200', function(test) {
     });
 
     casper.thenOpen('http://localhost:8888/categories/', function() {
-        test.assertHttpStatus(200, 'The categories page shoul load');
+        test.assertHttpStatus(200, 'The categories page should load');
     });
 
     casper.run(function() {
