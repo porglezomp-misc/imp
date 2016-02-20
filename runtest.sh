f7886dcb06ac02da9fe6356d8e75065b02a198e4
Caleb Jones <self@calebjones.net>
2016-02-20 00:44:15 -0500

    Make runtest wait for the server to start
    Instead of having a fixed startup time of 1 second, we can use curl to
    check if the server is responding to requests. We wait 0.1 seconds
    between request attempts, and run the test suite as soon as we get a
    successful response. If we don't get a response after 5 seconds, then
    we give up.

--- runtest.sh
+++ runtest.sh
@@ -9,6 +9,21 @@ DBNAME="db/test/$(date -Ins | sed 's|T|/|; s|,|/|').db"
 python server.py -d "$DBNAME" &> test_server.log
 ) &
 
-sleep 1
+LOADED=false
+for iter in {1..50}; do
+    # If we successfully make a connection, we're done
+    if curl localhost:8888 &> /dev/null; then
+        LOADED=true
+        break
+    fi
+    sleep 0.1
+done
+
+# If none of the connection attempts ever succeeded, then
+# we should quit without attempting to run the test suite
+if [ ! $LOADED ]; then
+    echo "Connection to localhost:8888 failing after 5 seconds, giving up!"
+    exit 1
+fi
 
 casperjs test tests/
